cd914039fadc41722e2e0a34d7300d05f2888793
allsers <mail@aleksanderekman.no>
2025-02-13 14:09:42 +0100

    removed demo images

--- test.py
+++ test.py
@@ -9,20 +9,6 @@ from diffusers import (
     StableDiffusionPipeline,
 )
 
-
-# We'll use a couple of demo images later in the notebook
-def download_image(url):
-    response = requests.get(url)
-    return Image.open(BytesIO(response.content)).convert("RGB")
-
-
-# Download images for inpainting example
-img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
-mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"
-
-init_image = download_image(img_url).resize((512, 512))
-mask_image = download_image(mask_url).resize((512, 512))
-
 device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
 
 model_id = "stabilityai/stable-diffusion-2-1-base"
