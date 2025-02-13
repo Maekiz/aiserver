3d805ad709ca1ad7b90e0753aa41d30fad52481d
allsers <mail@aleksanderekman.no>
2025-02-13 15:11:10 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,40 @@
 import torch
-from diffusers import StableDiffusion3Pipeline
-from diffusers import BitsAndBytesConfig, SD3Transformer2DModel
+from diffusers import StableDiffusion3Pipeline, SD3Transformer2DModel
+from diffusers import BitsAndBytesConfig
 
 torch.cuda.empty_cache()
 model_id = "stabilityai/stable-diffusion-3.5-large"
 
+# NF4 configuration
 nf4_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.bfloat16
 )
-model_nf4 = SD3Transformer2DModel.from_pretrained(
-    model_id,
-    subfolder="transformer",
+
+# Load the transformer with NF4 quantization
+transformer = SD3Transformer2DModel.from_single_file(
+    "https://huggingface.co/stabilityai/stable-diffusion-3.5-large/blob/main/sd3.5_large.safetensors",
     quantization_config=nf4_config,
-    torch_dtype=torch.bfloat16
+    torch_dtype=torch.bfloat16,
 )
 
 pipeline = StableDiffusion3Pipeline.from_pretrained(
-    model_id, 
-    transformer=model_nf4,
+    model_id,
+    transformer=transformer,
     torch_dtype=torch.bfloat16
 )
 pipeline.enable_model_cpu_offload()
 
-prompt = "A whimsical and creative image depicting a hybrid creature that is a mix of a waffle and a hippopotamus, basking in a river of melted butter amidst a breakfast-themed landscape. It features the distinctive, bulky body shape of a hippo. However, instead of the usual grey skin, the creature's body resembles a golden-brown, crispy waffle fresh off the griddle. The skin is textured with the familiar grid pattern of a waffle, each square filled with a glistening sheen of syrup. The environment combines the natural habitat of a hippo with elements of a breakfast table setting, a river of warm, melted butter, with oversized utensils or plates peeking out from the lush, pancake-like foliage in the background, a towering pepper mill standing in for a tree.  As the sun rises in this fantastical world, it casts a warm, buttery glow over the scene. The creature, content in its butter river, lets out a yawn. Nearby, a flock of birds take flight"
+prompt = "A whimsical and creative image depicting a hybrid creature that is a mix of a waffle and a hippopotamus, basking in a river of melted butter amidst a breakfast-themed landscape..."
 width = 1024
 height = 1024
 
 image = pipeline(
     prompt=prompt,
     num_inference_steps=28,
-    guidance_scale=7,
-    max_sequence_length=512,
-    width=width, 
-    height=height  
+    guidance_scale=3.5,
+    width=width,
+    height=height
 ).images[0]
-image.save("whimsical.png")
+image.save("output.png")
