593d764f961746df4efb197f478bfd0449b08a19
allsers <mail@aleksanderekman.no>
2025-02-13 14:27:07 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
 import torch
-import requests
-from PIL import Image
-from io import BytesIO
-from matplotlib import pyplot as plt
-
-# We'll be exploring a number of pipelines today!
-from diffusers import (
-    StableDiffusion3Pipeline
-)
-
-
-device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
+from diffusers import StableDiffusion3Pipeline
 
 pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16)
 pipe = pipe.to("cuda")
