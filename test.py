9a01687bbf51c114f65297dde422977f18fdadc6
allsers <mail@aleksanderekman.no>
2025-02-13 14:25:12 +0100

    stablediffusion3pipeline

--- test.py
+++ test.py
@@ -6,26 +6,18 @@ from matplotlib import pyplot as plt
 
 # We'll be exploring a number of pipelines today!
 from diffusers import (
-    StableDiffusionPipeline,
+    StableDiffusion3Pipeline
 )
+
 
 device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
 
-model_id = "stabilityai/stable-diffusion-2-1-base"
-pipe = StableDiffusionPipeline.from_pretrained(model_id).to(device)
+pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16)
+pipe = pipe.to("cuda")
 
-generator = torch.Generator(device=device).manual_seed(42)
-
-# Run the pipeline, showing some of the available arguments
-pipe_output = pipe(
-    prompt="Palette knife painting of an autumn cityscape",  # What to generate
-    negative_prompt="Oversaturated, blurry, low quality",  # What NOT to generate
-    height=480,
-    width=640,  # Specify the image size
-    guidance_scale=8,  # How strongly to follow the prompt
-    num_inference_steps=3,  # How many steps to take
-    generator=generator,  # Fixed random seed
-)
-
-# View the resulting image
-pipe_output.images[0]
+image = pipe(
+    "A capybara holding a sign that reads Hello World",
+    num_inference_steps=28,
+    guidance_scale=3.5,
+).images[0]
+image.save("capybara.png")
