f7444f80a1dcf7b267f5e0ae987d91cc929f8948
Maekiz <manoa039@osloskolen.no>
2025-02-14 12:35:21 +0100

    fungerende ai setup

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import torch
 from diffusers import StableDiffusion3Pipeline
+from huggingface_hub import login
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv('HUG_TOKEN')
+login(token)
 
 pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large-turbo", torch_dtype=torch.bfloat16)
 pipe = pipe.to("cuda")
@@ -9,10 +17,11 @@ height = 1024
 
 prompt="Palette knife painting of an autumn cityscape"
 
-image = pipeline(
+image = pipe(
     prompt=prompt,
-    num_inference_steps=28,
+    num_inference_steps=2,
     guidance_scale=7,
     width=width,
     height=height
 ).images[0]
+image.save('output.PNG')
