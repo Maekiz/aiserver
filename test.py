4867e34cbf989cf71eb57fcd154d4bdb61e3798e
Maekiz <manoa039@osloskolen.no>
2025-02-28 15:00:26 +0100

    debug satt til true

--- test.py
+++ test.py
@@ -75,4 +75,4 @@ async def generate():
         # return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
