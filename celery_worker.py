4da296b77dfe192cae5e84a6d3ac5ade2ac5e5c5
Maekiz <manoa039@osloskolen.no>
2025-03-04 14:18:26 +0100

    ny celery test

--- celery_worker.py
+++ celery_worker.py
@@ -4,48 +4,44 @@ from diffusers import BitsAndBytesConfig, SD3Transformer2DModel, StableDiffusion
 from transformers import T5EncoderModel
 import torch
 
+multiprocessing.set_start_method('spawn', force=True)
+
 celery = Celery('tasks', broker='redis://localhost:6379/0')
 
-# Ensure each task re-initializes CUDA
-def init_model():
-    model_id = "stabilityai/stable-diffusion-3.5-large-turbo"
+model_id = "stabilityai/stable-diffusion-3.5-large-turbo"
 
-    nf4_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
-    )
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
 
-    # Load models
-    print("Loading transformer model...")
-    model_nf4 = SD3Transformer2DModel.from_pretrained(
-        model_id,
-        subfolder="transformer",
-        quantization_config=nf4_config,
-        torch_dtype=torch.bfloat16
-    )
-    print("Loading text encoder model...")
-    t5_nf4 = T5EncoderModel.from_pretrained("diffusers/t5-nf4", torch_dtype=torch.bfloat16)
+# Load models
+print("Loading transformer model...")
+model_nf4 = SD3Transformer2DModel.from_pretrained(
+    model_id,
+    subfolder="transformer",
+    quantization_config=nf4_config,
+    torch_dtype=torch.bfloat16
+)
+print("Loading text encoder model...")
+t5_nf4 = T5EncoderModel.from_pretrained("diffusers/t5-nf4", torch_dtype=torch.bfloat16)
 
-    print("Initializing pipeline...")
-    pipeline = StableDiffusion3Pipeline.from_pretrained(
-        model_id,
-        transformer=model_nf4,
-        text_encoder_3=t5_nf4,
-        torch_dtype=torch.bfloat16
-    )
-    pipeline.enable_model_cpu_offload()
 
-    return pipeline
+print("Initializing pipeline...")
+pipeline = StableDiffusion3Pipeline.from_pretrained(
+    model_id,
+    transformer=model_nf4,
+    text_encoder_3=t5_nf4,
+    torch_dtype=torch.bfloat16
+)
+pipeline.enable_model_cpu_offload()
 
 @celery.task(bind=True)
 def worker(self, prompt, num_steps, guidance_scale, max_seq_length, userHeight, userWidth):
     try:
         print(f"Generating image for prompt: {prompt}")
         print(f"{userWidth}x{userHeight}")
-
-        # Initialize the model and pipeline for this task
-        pipeline = init_model()
 
         image = pipeline(
             prompt=prompt,
