f505dfec70068aa48b6cbbd6759b385e8e7c4691
Maekiz <manoa039@osloskolen.no>
2025-03-04 14:06:15 +0100

    ny celery multiprocess

--- celery_worker.py
+++ celery_worker.py
@@ -1,12 +1,14 @@
+import multiprocessing
 from celery import Celery
 from diffusers import BitsAndBytesConfig, SD3Transformer2DModel, StableDiffusion3Pipeline
 from transformers import T5EncoderModel
 import torch
-import os
+
+# Set the start method to 'spawn' to avoid the CUDA issue with forking
+multiprocessing.set_start_method('spawn', force=True)
 
 celery = Celery('tasks', broker='redis://localhost:6379/0')
 
-# Model configuration
 model_id = "stabilityai/stable-diffusion-3.5-large-turbo"
 
 nf4_config = BitsAndBytesConfig(
@@ -23,9 +25,9 @@ model_nf4 = SD3Transformer2DModel.from_pretrained(
     quantization_config=nf4_config,
     torch_dtype=torch.bfloat16
 )
-
 print("Loading text encoder model...")
 t5_nf4 = T5EncoderModel.from_pretrained("diffusers/t5-nf4", torch_dtype=torch.bfloat16)
+
 
 print("Initializing pipeline...")
 pipeline = StableDiffusion3Pipeline.from_pretrained(
@@ -36,14 +38,12 @@ pipeline = StableDiffusion3Pipeline.from_pretrained(
 )
 pipeline.enable_model_cpu_offload()
 
-# Define the Celery worker task
 @celery.task(bind=True)
 def worker(self, prompt, num_steps, guidance_scale, max_seq_length, userHeight, userWidth):
     try:
         print(f"Generating image for prompt: {prompt}")
         print(f"{userWidth}x{userHeight}")
 
-        # Generate image using the pipeline
         image = pipeline(
             prompt=prompt,
             num_inference_steps=num_steps,
@@ -52,11 +52,8 @@ def worker(self, prompt, num_steps, guidance_scale, max_seq_length, userHeight, 
             height=userHeight,
             width=userWidth
         ).images[0]
-
-        # Save the generated image
-        output_dir = './generated_images'
-        os.makedirs(output_dir, exist_ok=True)  # Ensure the directory exists
-        output_path = os.path.join(output_dir, f"generated_image_{self.request.id}.png")
+        
+        output_path = f"generated_image_{self.request.id}.png"
         image.save(output_path)
 
         return {"status": "completed", "file_path": output_path}
