ba5c5e80868a846992624fba3ee2f9c27b1aac75
Maekiz <manoa039@osloskolen.no>
2025-03-07 14:57:24 +0100

    ny debug

--- app.py
+++ app.py
@@ -39,8 +39,6 @@ def generate():
         # Start Celery task
         task = worker.delay(prompt, num_steps, guidance_scale,max_seq_length, userHeight, userWidth)
 
-        print('message: ', task.id, task.status)
-
         return jsonify({"message": "Task started", "task_id": task.id}), 202
 
     except Exception as e:
